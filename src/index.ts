04c6807991e584a4740258e2c76ead8911732e32
Tangent 128 <Tangent128@gmail.com>
2016-12-18 11:07:03 -0500

    UPSTREAM promote pin system to basecode

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@
 /// <reference path="ecs.ts" />
 /// <reference path="render.ts" />
 /// <reference path="ecs-common.ts" />
+/// <reference path="ecs-pin.ts" />
 /// <reference path="render-debug.ts" />
